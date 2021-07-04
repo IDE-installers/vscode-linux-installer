77abab558de22712aca854fe9f800d023497ee95
1RaY-1 <78962948+1RaY-1@users.noreply.github.com>
2021-07-04 17:11:19 +0200

    improved vscode-installer.sh

--- vscode-installer.sh
+++ vscode-installer.sh
@@ -5,33 +5,49 @@
 # https://linuxhint.com/install_visual_studio_code_arch_linux/
 ###
 
-# function to configure the installer
+# check what package manager user uses
 function configure(){
-    printf "Visual Studio Code installer for Linux\n"
-    
-    # list of valid options
-    LPM=("snap" "apt" "pacman" "yum" "dnf" "zypper" "nix") # [L]ist of [P]ackage [M]anagers 
 
-    printf "\nWhich package manager are you using?\n"
-    for eachPM in "${LPM[@]}"; do echo $eachPM; done
-    echo
+    if [ -f /bin/snap ]; then
+        PM=snap # [P]ackage[M]anager
 
-    read PM # [P]ackage [M]anager
+    elif [ -f /bin/apt ]; then
+        PM=apt
+
+    elif [ -f /bin/pacman ]; then
+        PM=pacman
+
+    elif [ -f /bin/dnf ]; then
+        PM=dnf
+
+    elif [ -f /bin/yum ]; then
+        PM=yum
+
+    elif [ -f /bin/zypper ]; then
+        PM=zypper
+
+    elif [ -f /bin/nix ]; then
+        PM=nix
+
+    else 
+        echo "Sorry, I don't know what to do"
+        exit 1
+    fi
 
 }
 
-# function to begin the instalation
+# function to begin the installation
 function install(){
     printf "\nInstalling VSCode...\n"
     set -e
 
     case $PM in
 
-        SNAP | snap )
+        snap )
             sudo snap install --classic code # or code-insiders
             ;;
 
-        APT | apt)
+        apt)
             wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
             sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
             sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
@@ -41,28 +57,28 @@ function install(){
             sudo apt install code # or code-insiders
             ;;
 
-        DNF | dnf)
+        dnf)
             sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
             sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
             sudo dnf check-update
             sudo dnf install code
             ;;
 
-        YUM | yum)
+        yum)
             sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
             sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
             sudo yum check-update
             sudo yum install code
             ;;
 
-        ZYPPER | zypper)
+        zypper)
             sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
             sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/zypp/repos.d/vscode.repo'
             sudo zypper refresh
             sudo zypper install code
             ;;
 
-        PACMAN | pacman)
+        pacman)
             git clone https://aur.archlinux.org/visual-studio-code-bin.git
             cd visual-studio-code-bin/
             makepkg -s
@@ -70,13 +86,13 @@ function install(){
             cd ../ && sudo rm -rf visual-studio-code-bin/
             ;;
 
-        NIX | nix)
+        nix)
             nix-env -i vscode
             ;;
 
-        *) echo "${PM} is invalid choice!";exit 1;;
     esac
 }
 
+printf "Visual Studio Code installer for Linux\n"
 configure
 install
