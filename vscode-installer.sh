35c7481d6c3ca459a6cd5b733f7cbe60c0bf7f9f
1RaY-1 <78962948+1RaY-1@users.noreply.github.com>
2024-01-05 22:16:59 +0100

    Update vscode-installer.sh
    Hopefully all of that works properly, because didn't test that actually, but the code is taken from the official microsoft site.
    Except for the Arch's code, I took it from another site, because apparently there  are no official binaries for Arch

--- vscode-installer.sh
+++ vscode-installer.sh
@@ -50,27 +50,28 @@ function install(){
             ;;
 
         apt)
+            sudo apt-get install wget gpg
             wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-            sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-            sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+            sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+            sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
             rm -f packages.microsoft.gpg
-            sudo apt install apt-transport-https
+            sudo apt install apt-transport-https -y
             sudo apt update
-            sudo apt install code # or code-insiders
+            sudo apt install code -y
             ;;
 
         dnf)
             sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
             sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
             sudo dnf check-update
-            sudo dnf install code
+            sudo dnf install code -y
             ;;
 
         yum)
             sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
             sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
             sudo yum check-update
-            sudo yum install code
+            sudo yum install code -y
             ;;
 
         zypper)
@@ -81,7 +82,9 @@ function install(){
             ;;
 
         pacman)
-            git clone https://aur.archlinux.org/visual-studio-code-bin.git
+#           Check if git is installed
+            ! [[ `command -v git` ]] && sudo pacman -S git --noconfirm
+            git clone https://AUR.archlinux.org/visual-studio-code-bin.git
             cd visual-studio-code-bin/
             makepkg -s
             sudo pacman -U visual-studio-code-bin-*.pkg.tar.xz
