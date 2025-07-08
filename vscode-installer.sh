6d8a6e69edac978003aac3aa4926495a10abf16d
1RaY-1 <78962948+1RaY-1@users.noreply.github.com>
2025-07-08 07:46:24 +0200

    Update vscode-installer.sh

--- vscode-installer.sh
+++ vscode-installer.sh
@@ -63,6 +63,7 @@ function install(){
             ;;
 
         yum)
+#           stable 64-bit VS Code for RHEL, Fedora, or CentOS
             sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
             echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 
