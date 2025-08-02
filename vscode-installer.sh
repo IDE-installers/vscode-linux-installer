dad56086dfd5b12417d3fc7e7fb80dea35386b5e
1RaY-1 <78962948+1RaY-1@users.noreply.github.com>
2025-08-02 09:54:41 +0200

    Stay up to date with the official install commands

--- vscode-installer.sh
+++ vscode-installer.sh
@@ -41,40 +41,51 @@ function install(){
     case $PM in
 
         apt)
-            sudo apt-get install wget gpg
-            wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-            sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-            echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
-            rm -f packages.microsoft.gpg
+            sudo apt-get install wget gpg -y
+            wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+            sudo install -D -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/microsoft.gpg
+            rm -f microsoft.gpg
 
-            sudo apt install apt-transport-https
+            sudo echo "Types: deb
+URIs: https://packages.microsoft.com/repos/code
+Suites: stable
+Components: main
+Architectures: amd64,arm64,armhf
+Signed-By: /usr/share/keyrings/microsoft.gpg" > /etc/apt/sources.list.d/vscode.sources
+
+            sudo apt install apt-transport-https -y
             sudo apt update
-            sudo apt install code # or code-insiders
+            sudo apt install code -y # or code-insiders
             ;;
 
         dnf)
-#           stable 64-bit VS Code for RHEL, Fedora, or CentOS
+#           Stable 64-bit VS Code for RHEL, Fedora, or CentOS (Stream)
             sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
             echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+
 
             dnf check-update
-            sudo dnf install code # or code-insiders
+            sudo dnf install code -y # or code-insiders
+
 
             ;;
-
+#           You can manually download and install the VS Code .rpm package (64-bit)
+#           however, auto-updating won't work unless the repository above is installed
         yum)
-#           stable 64-bit VS Code for RHEL, Fedora, or CentOS
+#           For older versions using YUM
             sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
             echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 
+
             yum check-update
-            sudo yum install code # or code-insiders
+            sudo yum install code -y # or code-insiders
             ;;
 
         zypper)
             sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
             echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" |sudo tee /etc/zypp/repos.d/vscode.repo > /dev/null
 
+#            User will have to confirm with "Y"
             sudo zypper install code
             ;;
 
